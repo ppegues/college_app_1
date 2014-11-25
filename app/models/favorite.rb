2d530f39c28e3ac88151ce4936f6b0a812de9f9a
ppegues <pacepegues@Pace-Peguess-MacBook-Pro.local>
2014-11-24 21:29:14 -0600

    validations

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ActiveRecord::Base
   belongs_to :profile
   belongs_to :school
+  validates :school_name, :uniqueness => true
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ActiveRecord::Base
   has_many :profiles, :through => :favorites
+  validates :name, :uniqueness => true
 end
